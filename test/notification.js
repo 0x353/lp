2c9caf6f410cdc5028bb1f3dfe1b322c1a0b9074
lottochain99 <lottochain99@gmail.com>
2025-04-09 16:47:08 +0700

    Update notification.js

--- test/notification.js
+++ test/notification.js
@@ -2,19 +2,19 @@ document.getElementById("notificationArea").innerHTML = `
   <div id="notificationPopup" class="popup hidden" role="alert">
     <button id="closePopup" onclick="closePopup()">×</button>
     <p id="notificationMessage"></p>
-  </div>
+</div>
 
-  <div id="notificationIcon" onclick="toggleNotificationList()">
+<div id="notificationIcon" onclick="toggleNotificationList()">
     <img src="img/bell.svg" alt="Notifications">
     <span id="notificationBadge" class="hidden"></span>
-  </div>
+</div>
 
-  <div id="notificationList" class="hidden">
+<div id="notificationList" class="hidden">
     <div class="notification-header">
-      <p>Notifications</p>
-      <ul id="notifications" aria-live="polite"></ul>
-      <p id="noNotificationsMessage" class="hidden">No notifications</p>
+        <p>Notifications</p>
+        <ul id="notifications" aria-live="polite"></ul>
+        <p id="noNotificationsMessage" class="hidden">No notifications</p>
     </div>
     <button onclick="closeNotificationList()">Close</button>
-  </div>
+</div>
 `;
