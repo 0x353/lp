b99ab96fc181065f44e025c44c35527411433060
lottochain99 <lottochain99@gmail.com>
2025-04-10 12:32:27 +0700

    Update XyZaPxgfxw.js

--- statis/XyZaPxgfxw.js
+++ statis/XyZaPxgfxw.js
@@ -242,37 +242,40 @@ footer {
 }
 
 .modal {
-    display: none;
-    position: fixed;
-    z-index: 999;
+    display: none; 
+    position: fixed; 
+    z-index: 999; 
     left: 0;
     top: 0;
     width: 100%;
     height: 100%;
     overflow: auto;
-    background-color: rgba(0, 0, 0, 0.7);
-    }
+    background-color: rgba(0,0,0,0.6);
+}
 
 .modal-content {
-background: #111;
-color: white;
-position: absolute;
-top: 50%;
-left: 50%;
-transform: translate(-50%, -50%);
-padding: 20px;
-width: 80%;
-max-width: 900px;
-border-radius: 10px;
-text-align: center;
-box-shadow: 0px 0px 15px rgba(0, 255, 255, 0.5); }
+    background-color: #111;
+    margin: 5% auto;
+    padding: 20px;
+    border-radius: 10px;
+    width: 90%;
+    max-width: 600px;
+    max-height: 90vh;
+    overflow-y: auto;
+    position: relative;
+}
 
 .close {
-  color: white;
-  float: right;
-  font-size: 24px;
-  cursor: pointer;
+    color: #aaa;
+    position: absolute;
+    top: 10px;
+    right: 20px;
+    font-size: 28px;
+    font-weight: bold;
+    cursor: pointer;
+    z-index: 1000;
 }
+
 .close:hover {
   color: red;
 }
